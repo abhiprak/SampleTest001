c0c9d1c180e6c308f7252741ed33ad3a792606fb
abhinav <abhinavprakash824@gmail.com>
2022-07-16 01:16:20 +0530

    Again committed

--- src/main/java/com/main/sampleproj001/LoginPage.java
+++ src/main/java/com/main/sampleproj001/LoginPage.java
@@ -4,6 +4,7 @@ public class LoginPage {
 
 	public static void main(String[] args) {
 		System.out.println("I am Login Page");
+		System.out.println("This is the updated code");
 	}
 
 }
---
+++ src/main/java/com/main/sampleproj001/OrderPage.java
@@ -0,0 +1,10 @@
+package com.main.sampleproj001;
+
+public class OrderPage {
+	
+	public static void main(String[] args) {
+		System.out.println("I am Orders Page");
+	}
+
+
+}
